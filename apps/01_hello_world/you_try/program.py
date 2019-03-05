d24f884f85fc6bb5cc01c1d6816e35d330542716
dspec12 <danspec12@gmail.com>
2019-03-05 15:30:11 -0500

    app 01 complete

---
+++ apps/01_hello_world/you_try/program.py
@@ -0,0 +1,9 @@
+print('-------------------------------------')
+print('            HELLO APP')
+print('-------------------------------------')
+print()
+
+user_input = input('What is your name? ')
+greeting = "Nice to meet you, " + user_input
+
+print(greeting)
